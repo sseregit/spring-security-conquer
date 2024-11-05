8e93bc3b073e5d8c55dea2b51cabd9fa5da95053
장연식 <sseregit@knowwheresoft.com>
2024-11-05 16:55:18 +0900

    settings

---
+++ src/main/java/spring/security/conquer/ConquerApplication.java
@@ -0,0 +1,13 @@
+package spring.security.conquer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConquerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConquerApplication.class, args);
+    }
+
+}
