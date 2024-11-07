a3d45be33201a0ddfbd1b9f6746c307b3b534885
장연식 <sseregit@knowwheresoft.com>
2024-11-07 17:57:15 +0900

    인증 – Authentication

--- src/main/java/spring/security/conquer/SecurityConfig.java
+++ src/main/java/spring/security/conquer/SecurityConfig.java
@@ -1,22 +1,14 @@
 package spring.security.conquer;
 
-import jakarta.servlet.ServletException;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
-import org.springframework.security.web.savedrequest.HttpSessionRequestCache;
-import org.springframework.security.web.savedrequest.SavedRequest;
-
-import java.io.IOException;
 
 @EnableWebSecurity
 @Configuration
@@ -25,22 +17,10 @@ class SecurityConfig {
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
-        HttpSessionRequestCache requestCache = new HttpSessionRequestCache();
-        requestCache.setMatchingRequestParameterName("customParam=y");
-
         http.authorizeRequests(auth -> auth
                         .requestMatchers("/logout-success").permitAll()
                         .anyRequest().authenticated())
-                .formLogin(form -> form
-                        .successHandler(new AuthenticationSuccessHandler() {
-                            @Override
-                            public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
-                                SavedRequest savedRequest = requestCache.getRequest(request, response);
-                                String targetUrl = savedRequest.getRedirectUrl();
-                                response.sendRedirect(targetUrl);
-                            }
-                        }))
-                .requestCache(cache -> cache.requestCache(requestCache))
+                .formLogin(Customizer.withDefaults())
         ;
 
         return http.build();
