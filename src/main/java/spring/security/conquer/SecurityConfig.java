41791b54945fca639c145f3cf58d686e39fc60a6
장연식 <sseregit@knowwheresoft.com>
2024-11-11 16:35:20 +0900

    SecurityContextRepository / SecurityContextHolderFilter - 3

--- src/main/java/spring/security/conquer/SecurityConfig.java
+++ src/main/java/spring/security/conquer/SecurityConfig.java
@@ -2,11 +2,16 @@ package spring.security.conquer;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @EnableWebSecurity
 @Configuration
@@ -15,21 +20,25 @@ class SecurityConfig {
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
+        AuthenticationManagerBuilder builder = http.getSharedObject(AuthenticationManagerBuilder.class);
+        AuthenticationManager authenticationManager = builder.build();
 
         http.authorizeRequests(auth -> auth
+                        .requestMatchers("/api/login").permitAll()
                         .anyRequest().authenticated())
                 .formLogin(Customizer.withDefaults())
-        ;
-
+//                .securityContext(securityContext -> securityContext.requireExplicitSave(false))
+                .authenticationManager(authenticationManager)
+                .addFilterBefore(customAuthenticationFilter(http, authenticationManager), UsernamePasswordAuthenticationFilter.class);
         return http.build();
     }
 
-    @Bean
-    UserDetailsService userDetailsService() {
-        return new CustomUserDetailsService();
+    CustomAuthenticationFilter customAuthenticationFilter(HttpSecurity http, AuthenticationManager authenticationManager) {
+        CustomAuthenticationFilter customAuthenticationFilter = new CustomAuthenticationFilter(http);
+        customAuthenticationFilter.setAuthenticationManager(authenticationManager);
+        return customAuthenticationFilter;
     }
 
-/*
     @Bean
     InMemoryUserDetailsManager inMemoryUserDetailsManager() {
         UserDetails user = User.withUsername("user")
@@ -38,6 +47,5 @@ class SecurityConfig {
                 .build();
         return new InMemoryUserDetailsManager(user);
     }
-*/
 
 }
