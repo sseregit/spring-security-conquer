2694f5f76c9f515a72b5d16ca6cb6714f0dcc1d6
장연식 <sseregit@knowwheresoft.com>
2024-11-11 11:54:25 +0900

    사용자 상세 - UserDetails

---
+++ src/main/java/spring/security/conquer/CustomUserDetails.java
@@ -0,0 +1,28 @@
+package spring.security.conquer;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+
+@RequiredArgsConstructor
+public class CustomUserDetails implements UserDetails {
+
+    private final AccountDto accountDto;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return accountDto.getAuthorities();
+    }
+
+    @Override
+    public String getPassword() {
+        return accountDto.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return accountDto.getUsername();
+    }
+}
--- src/main/java/spring/security/conquer/CustomUserDetailsService.java
+++ src/main/java/spring/security/conquer/CustomUserDetailsService.java
@@ -1,16 +1,18 @@
 package spring.security.conquer;
 
-import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
+import java.util.List;
+
 public class CustomUserDetailsService implements UserDetailsService {
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return User.withUsername("user")
-                .password("{noop}1111")
-                .authorities("ROLE_USER")
-                .build();
+
+        AccountDto accountDto = new AccountDto("user", "{noop}1111", List.of(new SimpleGrantedAuthority("ROLE_USER")));
+
+        return new CustomUserDetails(accountDto);
     }
 }
