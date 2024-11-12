f54847029be4781a5ac9d09fe0a77f0453556677
장연식 <sseregit@knowwheresoft.com>
2024-11-12 11:03:54 +0900

    세션 고정 보호 - sessionManagement().sessionFixation()

--- src/main/java/spring/security/conquer/SecurityConfig.java
+++ src/main/java/spring/security/conquer/SecurityConfig.java
@@ -2,18 +2,16 @@ package spring.security.conquer;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.Customizer;
-import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.SessionManagementConfigurer;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
-@EnableWebSecurity(debug = true)
+@EnableWebSecurity
 @Configuration
 class SecurityConfig {
 
@@ -21,17 +19,12 @@ class SecurityConfig {
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
         http.authorizeRequests(auth -> auth
-                        .requestMatchers("/login").permitAll()
                         .anyRequest().authenticated())
-//                .formLogin(Customizer.withDefaults())
-                .csrf(AbstractHttpConfigurer::disable)
+                .formLogin(Customizer.withDefaults())
+                .sessionManagement(session -> session
+                        .sessionFixation(sessionFixation -> sessionFixation.changeSessionId()))
         ;
         return http.build();
-    }
-
-    @Bean
-    AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
-        return configuration.getAuthenticationManager();
     }
 
     @Bean
