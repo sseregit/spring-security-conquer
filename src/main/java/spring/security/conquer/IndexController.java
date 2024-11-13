2ec4460fe54470f55347667a8c932818c959f8b8
sseregit <sseregit@naver.com>
2024-11-14 04:55:57 +0900

    요청 기반 권한 부여 - HttpSecurity.authorizeHttpRequests() - 2

--- src/main/java/spring/security/conquer/IndexController.java
+++ src/main/java/spring/security/conquer/IndexController.java
@@ -1,11 +1,5 @@
 package spring.security.conquer;
 
-import jakarta.servlet.http.HttpServletRequest;
-import org.springframework.security.authentication.AnonymousAuthenticationToken;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.annotation.CurrentSecurityContext;
-import org.springframework.security.core.context.SecurityContext;
-import org.springframework.security.web.csrf.CsrfToken;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,78 +7,43 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class IndexController {
 
-    @GetMapping("/")
-    public Authentication index(Authentication authentication) {
-        return authentication;
+    @GetMapping("/user")
+    public String user(){
+        return "user";
     }
 
-    @GetMapping("/login")
-    public String login() {
-        return "login";
+    @GetMapping("/myPage/points")
+    public String myPage(){
+        return "myPage";
     }
 
-    @GetMapping("/denied")
-    public String denied() {
-        return "denied";
+    @GetMapping("/manager")
+    public String manager(){
+        return "manager";
     }
 
-    @GetMapping("/loginPage")
-    public String loginPage() {
-        return "loginPage";
+    @GetMapping("/admin")
+    public String admin(){
+        return "admin";
     }
 
-    @GetMapping("/home")
-    public String home() {
-        return "home";
+    @GetMapping("/admin/payment")
+    public String adminPayment(){
+        return "adminPayment";
     }
 
-    @GetMapping("/anonymous")
-    public String anonymous() {
-        return "anonymous";
+    @GetMapping("/resource/address_01")
+    public String address_01(){
+        return "address_01";
     }
 
-    @GetMapping("/anonymous-context")
-    public String anonymousContext(@CurrentSecurityContext SecurityContext context) {
-        return context.getAuthentication().getName();
+    @GetMapping("/resource/address01")
+    public String address01(){
+        return "address01";
     }
 
-    @GetMapping("/authentication")
-    public String authentication(Authentication authentication) {
-
-        if (authentication instanceof AnonymousAuthenticationToken) {
-            return "anonymous";
-        } else {
-            return "not anonymous";
-        }
-    }
-
-    @GetMapping("/logout-success")
-    public String logoutSuccess() {
-        return "logout-success";
-    }
-
-    @PostMapping("/csrf")
-    String csrf() {
-        return "csrf 적용됨";
-    }
-
-    @PostMapping("/formCsrf")
-    CsrfToken formCsrf(CsrfToken csrfToken) {
-        return csrfToken;
-    }
-
-    @PostMapping("/cookieCsrf")
-    CsrfToken cookieCsrf(CsrfToken csrfToken) {
-        return csrfToken;
-    }
-
-    @GetMapping("/csrfToken")
-    String csrfToken(HttpServletRequest request) {
-        CsrfToken csrfToken1 = (CsrfToken) request.getAttribute(CsrfToken.class.getName());
-        CsrfToken csrfToken2 = (CsrfToken) request.getAttribute("_csrf");
-
-        String token = csrfToken1.getToken();
-
-        return token;
+    @PostMapping("/post")
+    public String post(){
+        return "post";
     }
 }
--- src/main/java/spring/security/conquer/SecurityConfig.java
+++ src/main/java/spring/security/conquer/SecurityConfig.java
@@ -2,25 +2,39 @@ package spring.security.conquer;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.servlet.util.matcher.MvcRequestMatcher;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.security.web.util.matcher.RegexRequestMatcher;
+import org.springframework.web.servlet.handler.HandlerMappingIntrospector;
 
 @EnableWebSecurity
 @Configuration
 class SecurityConfig {
 
     @Bean
-    SecurityFilterChain cookieSecurityFilterChain(HttpSecurity http) throws Exception {
+    SecurityFilterChain cookieSecurityFilterChain(HttpSecurity http, HandlerMappingIntrospector introspector) throws Exception {
 
         http.authorizeRequests(auth -> auth
-                        .requestMatchers("/csrf", "/csrfToken", "/form", "/formCsrf", "/cookie", "/cookieCsrf").permitAll()
+                        .requestMatchers("/").permitAll()
+                        .requestMatchers("/user").hasAuthority("ROLE_USER")
+                        .requestMatchers("/myPage/**").hasRole("USER")
+                        .requestMatchers(HttpMethod.POST).hasAuthority("ROLE_WRITE")
+                        .requestMatchers(new AntPathRequestMatcher("/manager/**")).hasAuthority("ROLE_MANAGER")
+                        .requestMatchers(new MvcRequestMatcher(introspector, "/admin/payment")).hasAuthority("ROLE_ADMIN")
+                        .requestMatchers("/admin/**").hasAnyAuthority("ROLE_ADMIN", "ROLE_MANAGER")
+                        .requestMatchers(new RegexRequestMatcher("/resource/[A-Za-z0-9]+", null)).hasAuthority("ROLE_MANAGER")
                         .anyRequest().authenticated())
                 .formLogin(Customizer.withDefaults())
+                .csrf(AbstractHttpConfigurer::disable)
         ;
         return http.build();
     }
@@ -31,7 +45,18 @@ class SecurityConfig {
                 .password("{noop}1111")
                 .authorities("ROLE_USER")
                 .build();
-        return new InMemoryUserDetailsManager(user);
+
+        UserDetails manager = User.withUsername("manager")
+                .password("{noop}1111")
+                .authorities("ROLE_MANAGER")
+                .build();
+
+        UserDetails admin = User.withUsername("admin")
+                .password("{noop}1111")
+                .authorities("ROLE_ADMIN", "ROLE_WRITE")
+                .build();
+
+        return new InMemoryUserDetailsManager(user, manager, admin);
     }
 
 }
