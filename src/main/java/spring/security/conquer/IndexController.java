7314ed448ff2b17d191e970cc1cc021d3f5d45f8
장연식 <sseregit@knowwheresoft.com>
2024-11-18 14:27:26 +0900

    다중 보안 설정

--- src/main/java/spring/security/conquer/IndexController.java
+++ src/main/java/spring/security/conquer/IndexController.java
@@ -1,21 +1,10 @@
 package spring.security.conquer;
 
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContext;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.concurrent.Callable;
-
 @RestController
 public class IndexController {
-
-    private final AsyncService asyncService;
-
-    public IndexController(AsyncService asyncService) {
-        this.asyncService = asyncService;
-    }
 
     @GetMapping("/")
     String index() {
@@ -37,28 +26,4 @@ public class IndexController {
         return "admin";
     }
 
-    @GetMapping("/callable")
-    Callable<Authentication> call() {
-        SecurityContext securityContext = SecurityContextHolder.getContextHolderStrategy().getContext();
-        println(securityContext);
-        return () -> {
-            SecurityContext securityContext1 = SecurityContextHolder.getContextHolderStrategy().getContext();
-            println(securityContext1);
-            return securityContext1.getAuthentication();
-        };
-    }
-
-    @GetMapping("/async")
-    Authentication async() {
-        SecurityContext securityContext1 = SecurityContextHolder.getContextHolderStrategy().getContext();
-        println(securityContext1);
-
-        asyncService.asyncMethod();
-
-        return securityContext1.getAuthentication();
-    }
-
-    private void println(SecurityContext securityContext) {
-        System.out.println("Thread = " + Thread.currentThread().getName() + "\nsecurityContext = " + securityContext);
-    }
 }
--- src/main/java/spring/security/conquer/SecurityConfig.java
+++ src/main/java/spring/security/conquer/SecurityConfig.java
@@ -3,13 +3,12 @@ package spring.security.conquer;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
@@ -28,19 +27,24 @@ class SecurityConfig {
     }
 
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests(authorize -> authorize
-                        .requestMatchers("/user").hasAuthority("ROLE_USER")
-                        .requestMatchers("/db").hasAuthority("ROLE_DB")
-                        .requestMatchers("/admin").hasAuthority("ROLE_ADMIN")
-                        .anyRequest().permitAll())
+                        .anyRequest().authenticated())
                 .formLogin(Customizer.withDefaults())
-                .csrf(AbstractHttpConfigurer::disable)
         ;
 
-        SecurityContextHolder.setStrategyName(SecurityContextHolder.MODE_INHERITABLETHREADLOCAL);
+        return http.build();
+    }
+
+    @Bean
+    @Order(1)
+    SecurityFilterChain securityFilterChain2(HttpSecurity http) throws Exception {
+        http
+                .securityMatchers(matchers -> matchers.requestMatchers("/api/**"))
+                .authorizeHttpRequests(authorize -> authorize
+                        .anyRequest().permitAll())
+        ;
 
         return http.build();
     }
