6d868dd635b0351710a2e9375a62704b4cd99bc7
장연식 <sseregit@knowwheresoft.com>
2024-11-18 17:02:52 +0900

    프로젝트 생성 및 기본 구성

---
+++ src/main/java/spring/security/conquer/controller/HomeController.java
@@ -0,0 +1,29 @@
+package spring.security.conquer.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+class HomeController {
+
+    @GetMapping("/")
+    String dashboard() {
+        return "/dashboard";
+    }
+
+    @GetMapping("/user")
+    String user() {
+        return "/user";
+    }
+
+    @GetMapping("/manager")
+    String manager() {
+        return "/manager";
+    }
+
+    @GetMapping("/admin")
+    String admin() {
+        return "/admin";
+    }
+
+}
