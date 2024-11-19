2116089a6896ce7d828be66d37c0835ad67005f0
sseregit <sseregit@naver.com>
2024-11-20 04:46:22 +0900

    Rest 로그아웃 구현

--- src/main/java/spring/security/conquer/user/controller/RestApiContorller.java
+++ src/main/java/spring/security/conquer/user/controller/RestApiContorller.java
@@ -1,6 +1,11 @@
 package spring.security.conquer.user.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -23,6 +28,17 @@ class RestApiContorller {
     @GetMapping("/admin")
     AccountDto restAdmin(@AuthenticationPrincipal AccountDto accountDto) {
         return accountDto;
+    }
+
+    @GetMapping("/logout")
+    String logout(HttpServletRequest request, HttpServletResponse response) {
+        Authentication authentication = SecurityContextHolder.getContextHolderStrategy().getContext().getAuthentication();
+
+        if (authentication != null) {
+            new SecurityContextLogoutHandler().logout(request, response, authentication);
+        }
+
+        return "logout";
     }
 
 }
