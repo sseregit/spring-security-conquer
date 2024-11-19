612255d30871579e9dad42733c35d236020b6cdf
장연식 <sseregit@knowwheresoft.com>
2024-11-19 10:59:04 +0900

    커스텀 로그아웃

--- src/main/java/spring/security/conquer/user/controller/LoginController.java
+++ src/main/java/spring/security/conquer/user/controller/LoginController.java
@@ -1,5 +1,10 @@
 package spring.security.conquer.user.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
@@ -15,4 +20,16 @@ class LoginController {
     String signup() {
         return "login/signup";
     }
+
+    @GetMapping("/logout")
+    String logout(HttpServletRequest request, HttpServletResponse response) {
+        Authentication authentication = SecurityContextHolder.getContextHolderStrategy().getContext().getAuthentication();
+
+        if (authentication != null) {
+            new SecurityContextLogoutHandler().logout(request, response, authentication);
+        }
+
+        return "redirect:/login";
+    }
+
 }
